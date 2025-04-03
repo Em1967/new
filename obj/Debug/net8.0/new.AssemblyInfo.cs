c4a778f7592f1e234f05a820d5b3bb9c90d46dd3
Elijah Michel <ElijahMichel2468@Icloud.com>
2025-04-03 13:25:32 -0500

    final commit

--- obj/Debug/net8.0/new.AssemblyInfo.cs
+++ obj/Debug/net8.0/new.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+543db29b6a5110ab16327aad593d458c7a9ea53b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb28a9fff09a09460c45913ce3f67413e7143665")]
 [assembly: System.Reflection.AssemblyProductAttribute("new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
