e015daa3db3f6898aa809c7698d634885d200fb5
Em1967 <ElijahMichel2468@icloud.com>
2025-04-06 22:04:11 -0500

    ^

--- obj/Debug/net8.0/new.AssemblyInfo.cs
+++ obj/Debug/net8.0/new.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5940f1681bf63b557f653d25d2823a8f55728ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46152c54a724a4e3aa150442de977d12a5d00e63")]
 [assembly: System.Reflection.AssemblyProductAttribute("new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
